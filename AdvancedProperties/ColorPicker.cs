2942c32fdbf699b14cc0c5e53110bc47c078e5c4
Kulpot <kulpotwahu@gmail.com>
2023-04-29 08:13:03 +0800

    Add project files.

---
+++ AdvancedProperties/ColorPicker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedProperties
+{
+    class ColorPicker : Control
+    {
+        private Color _pickedColor = Color.Black;
+        public event EventHandler PickedColorChanged;
+
+        protected virtual void OnPickedColorChanged()
+        {
+            if(PickedColorChanged != null)
+                PickedColorChanged(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        ///  Gets or Sets the picked color :) 
+        /// </summary>
+        [DefaultValue(typeof(Color), "Black")]
+        [Category("Input")]
+        [DisplayName("~~~~Picked Color~~~~")]
+        [Description("*****The color that has been picked*****")]
+        //[Obsolete("This is old!", false)]
+        //[Localizable(true)]//if properties is string, means that diff. states of properties for diff. locals
+        //[RefreshProperties(System.ComponentModel.RefreshProperties.All)]
+        //[Browsable(false)]
+        //[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Color PickedColor
+        {
+            get { return _pickedColor; }
+            set
+            {
+                if (value == Color.Transparent && value == Color.Magenta)
+                {
+                    return;
+                }
+
+                bool changed = value != _pickedColor;
+                _pickedColor = value;
+
+                if (changed)
+                    OnPickedColorChanged();
+            }
+        }
+    }
+}
---
+++ AdvancedProperties/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AdvancedProperties
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorPicker1 = new AdvancedProperties.ColorPicker();
+            this.SuspendLayout();
+            // 
+            // colorPicker1
+            // 
+            this.colorPicker1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.colorPicker1.Location = new System.Drawing.Point(46, 46);
+            this.colorPicker1.Name = "colorPicker1";
+            this.colorPicker1.Size = new System.Drawing.Size(158, 46);
+            this.colorPicker1.TabIndex = 0;
+            this.colorPicker1.Text = "colorPicker1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 268);
+            this.Controls.Add(this.colorPicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ColorPicker colorPicker1;
+    }
+}
+
